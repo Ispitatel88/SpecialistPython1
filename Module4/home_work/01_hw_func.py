b0c707ec1faca5119799097ca153b61c715ac4c8
Ispitatel88 <87034421+Ispitatel88@users.noreply.github.com>
2021-07-12 14:49:07 +0300

    Update 01_hw_func.py

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -7,7 +7,36 @@ def lucky_ticket(ticket_number):
     pass
 
 
-# Тестируем функцию
+# Вариант № 1
+
+def lucky_ticket(number):
+    num = 0
+    copy_number = number
+
+    while number > 0:
+        new_number = number%10
+        num = num*10 + new_number
+        number = number//10
+
+    print(num)
+
+    if num == copy_number:
+        return num and True
+    else:
+        return num and False
+
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
+
+# Вариант № 2
+
+def lucky_ticket_str(number):
+    number_str = str(number)
+    new_number = number_str[::-1]
+    if new_number == number_str:
+        return number, True # return tuple
+    else:
+        return number, False
+
+print(lucky_ticket_str(5555))
