78d3e705f619cc551506777596efe4ceedb35c38
Ispitatel88 <87034421+Ispitatel88@users.noreply.github.com>
2021-07-14 10:37:50 +0300

    Update 02_hw_file.py

--- Module6/home_work/02_hw_file.py
+++ Module6/home_work/02_hw_file.py
@@ -1,5 +1,13 @@
 # Дан файл data/info.txt, в каждой строке которого содержится строка или целое число
 # Найдите сумму всех чисел, пропуская все строки содержащие не числовые значения
-
-with open("data/info.txt", "r") as f:
-    pass
+import os
+path = os.path.join('files', 'info.txt')
+list_of_number = []
+with open(path, 'r', encoding='UTF-8') as f:
+    for line in f:
+        line = line.rstrip()
+        try:
+            if int(line):
+                list_of_number.append(int(line))
+        except: None
+print(sum(list_of_number))
