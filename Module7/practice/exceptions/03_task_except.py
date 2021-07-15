f57e671478d1e5ed6a478f0d0785ef981e724179
Ispitatel88 <87034421+Ispitatel88@users.noreply.github.com>
2021-07-15 17:22:09 +0300

    Update 03_task_except.py

--- Module7/practice/exceptions/03_task_except.py
+++ Module7/practice/exceptions/03_task_except.py
@@ -1,3 +1,4 @@
+
 # Дана строка из пяти целых чисел, разделенных пробелом.
 # Пример входных данных: "2 12 -45 7 10"
 # Напишите программу, которая находит среди них минимальное положительное число.
@@ -5,3 +6,19 @@
 
 # При решении задачи требуется учесть формат входных данных.
 # Если входные данные некорректные, сообщить об этом.
+
+# ВТОРОЙ ВАРИАНТ
+
+
+while True:
+    inp_str = input('Enter five numbers: ')
+    try:
+        l = inp_str.split()
+        if len(l) != 5:
+            raise ValueError
+        else:
+            min_2 = min(list(filter(lambda x:int(x)>0, l)))
+            break
+    except ValueError:
+        print('U enter incorrect data! Try again!')
+print(min_2)
