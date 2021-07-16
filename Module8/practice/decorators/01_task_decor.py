fb79a73aed42f367b1fd6be7e05364051b0b12e5
Ispitatel88 <87034421+Ispitatel88@users.noreply.github.com>
2021-07-16 13:58:21 +0300

    Update 01_task_decor.py

--- Module8/practice/decorators/01_task_decor.py
+++ Module8/practice/decorators/01_task_decor.py
@@ -7,3 +7,22 @@
 # *23*
 # ****
 # (кол-во звездочек зависит от длины возвращаемого значения)
+# def hello(word):
+#
+#     return word
+# print(hello('Hi it is me!'))
+def dec(hello):
+    def around(word):
+        # print('*'*len(word) + '\n*')
+        print ('*'*(len(word)+2))
+        print('*' + hello(word) + '*')
+        print('*' * (len(word) + 2))
+        #print('*' * len(word) + '\n*')
+    return around
+
+
+@dec
+def hello(word):
+    return (word)
+
+hello('Hello it is me!')
